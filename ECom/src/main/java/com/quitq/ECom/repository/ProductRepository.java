40398cb2e87d7c658f8e5fd7d80f2a4ed312bc43
Jai Kishore <91529122+JK-DooM@users.noreply.github.com>
2024-09-22 19:22:24 +0530

    Delete ECom directory

--- ECom/src/main/java/com/quitq/ECom/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quitq.ECom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.quitq.ECom.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product,Integer>{
-
-}
