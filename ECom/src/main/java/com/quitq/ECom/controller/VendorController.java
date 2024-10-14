a355f71c769081ce53a8068565a02c5a89f868ab
huda2810 <shaikhhuda2810@gmail.com>
2024-10-14 19:50:59 +0530

    Email api

--- ECom/src/main/java/com/quitq/ECom/controller/VendorController.java
+++ ECom/src/main/java/com/quitq/ECom/controller/VendorController.java
@@ -2,6 +2,7 @@ package com.quitq.ECom.controller;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,18 +10,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
 import com.quitq.ECom.Exception.InvalidIdException;
 import com.quitq.ECom.dto.MessageDto;
 import com.quitq.ECom.enums.Category;
 import com.quitq.ECom.model.User;
 import com.quitq.ECom.model.Vendor;
 import com.quitq.ECom.repository.UserRepository;
+import com.quitq.ECom.service.MyUserDetailsService;
 import com.quitq.ECom.service.VendorService;
 
 @RestController
@@ -30,8 +39,12 @@ import com.quitq.ECom.service.VendorService;
 public class VendorController {
 	@Autowired
 	VendorService vendorService;
+	@Autowired
+	AmazonSimpleEmailService service;
 	 @Autowired
 		private UserRepository userRepository;
+	 @Autowired
+	 private MyUserDetailsService userDetailsService;
 		
 		@Autowired
 		private PasswordEncoder passwordEncoder;
@@ -94,6 +107,34 @@ public class VendorController {
 	public List<Category> getAllCategory(){
 		return List.of(Category.values());
 	}
+	@GetMapping("/getRandomNumber")
+	public Integer getRandomNumber() {
+		Random rand = new Random();
+		  
+        // Generate random integers in range 0 to 999
+        int rand_int1 = rand.nextInt(1000);
+        return Integer.valueOf(rand_int1);
+	}
+	@GetMapping("/sendEmail/{number}")
+	public String sendEmail(@PathVariable int number){
+		Content content=new Content("Verification body");
+		Body body=new Body(new Content("This is verification mail body number="+number));
+		Message message=new Message(content,body);
+		SendEmailRequest mail=new SendEmailRequest("shaikhhuda2810@gmail.com",
+				new Destination().withToAddresses("0808cs201091.ies@ipsacademy.org"),message);
+		service.sendEmail(mail);
+		return "Email Sent";
+	}
+	@GetMapping("/resetPassword/{newPassword}")
+	public ResponseEntity<?> getOldPassword(Principal p,@PathVariable String newPassword){
+		String userName=p.getName();
+		Vendor v=vendorService.getVendorByUserName(userName);
+
+		User u=v.getUser();
+		u.setPassword(passwordEncoder.encode(newPassword));
+    	userRepository.save(u);	
+    	return ResponseEntity.ok(u);
+    	}
 	/*
 	@GetMapping("/product/{id}")
 	public ResponseEntity<?> getVendorByProductName(@PathVariable int id,MessageDto messageDto)
